063a4e7833dfa8332d2681c513b8209ca2cbe1d3
Ekrem05 <ekremnptg@gmail.com>
2024-06-13 21:56:28 +0300

    Redux package installed

--- filmdive.client/src/App.jsx
+++ filmdive.client/src/App.jsx
@@ -1,15 +1,17 @@
 import Navigation from "./components/Navigation/Navigation";
 import AboveTheFold from "./components/Header/AboveTheFold";
 import Header from "./components/Header/Header";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+const client = new QueryClient();
 function App() {
   return (
-    <>
+    <QueryClientProvider client={client}>
       <Header>
         <Navigation />
         <AboveTheFold />
       </Header>
       <h2>Hello</h2>
-    </>
+    </QueryClientProvider>
   );
 }
 
--- filmdive.client/src/components/Header/AboveTheFold.jsx
+++ filmdive.client/src/components/Header/AboveTheFold.jsx
@@ -4,32 +4,28 @@ import { aboveTheFoldAnimation } from "../../utils/animations";
 import { motion, AnimatePresence } from "framer-motion";
 import { getTrendingMovies } from "../../http/movies";
 import MovieInfo from "./MovieInfo";
+import { useQuery } from "@tanstack/react-query";
 export default function AboveTheFold() {
-  const [trendingMovies, setTrendingMovies] = useState([]);
-  useEffect(() => {
-    async function request() {
-      const movies = await getTrendingMovies();
-      setTrendingMovies(movies);
-      return movies;
-    }
-    request();
-  }, []);
+  const { isPending, isError, data, error } = useQuery({
+    queryKey: ["movies"],
+    queryFn: getTrendingMovies,
+  });
   return (
     <AnimatePresence>
-      {trendingMovies[0] && (
+      {data && (
         <section className="relative bg-primarydrk h-auto pb-8">
           <motion.img
-            key={trendingMovies[0].title}
+            key={data[0].title}
             {...aboveTheFoldAnimation}
             id="hero-img"
-            src={`https://image.tmdb.org/t/p/original/${trendingMovies[0].backdropPath}`}
+            src={`https://image.tmdb.org/t/p/original/${data[0].backdropPath}`}
             alt=""
           />
           <AnimatePresence>
             {" "}
             <section className="absolute w-[100%] pl-20 bottom-0 mt-44 mb-10 flex flex-col gap-5">
-              <MovieInfo movie={trendingMovies[0]} />
-              <Trending movies={trendingMovies} />
+              <MovieInfo movie={data[0]} />
+              <Trending movies={data} />
             </section>
           </AnimatePresence>
         </section>
