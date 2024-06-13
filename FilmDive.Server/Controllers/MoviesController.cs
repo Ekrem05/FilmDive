d351fe5fd76faff0d09a895000587d11e4c11868
Ekrem05 <ekremnptg@gmail.com>
2024-06-13 21:36:14 +0300

    Endpoint for trending movies added

---
+++ FilmDive.Server/Controllers/MoviesController.cs
@@ -0,0 +1,23 @@
+﻿using FilmDive.Server.Services.Movies;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmDive.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMovieService movieService;
+
+        public MoviesController(IMovieService _movieService)
+        {
+            movieService = _movieService;
+        }
+
+        [HttpGet(Name ="trending")]
+        public async Task<IActionResult> GetTrendingMovies()
+        {
+            return Ok(await movieService.GetTrendingMoviesAsync());
+        }
+    }
+}
