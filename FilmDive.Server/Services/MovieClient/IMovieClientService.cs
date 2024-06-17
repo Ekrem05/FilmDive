d2d546050d13811d02355861944855a486489409
Ekrem05 <ekremnptg@gmail.com>
2024-06-17 15:57:49 +0300

    Http requests to the fmdb api are now in a service

---
+++ FilmDive.Server/Services/MovieClient/IMovieClientService.cs
@@ -0,0 +1,7 @@
+﻿namespace FilmDive.Server.Services.MovieClient
+{
+    public interface IMovieClientService
+    {
+        Task<string> SendRequestAsync(string uri, string apiKey);
+    }
+}
---
+++ FilmDive.Server/Services/MovieClient/MovieClientService.cs
@@ -0,0 +1,34 @@
+﻿
+using Microsoft.Extensions.Configuration;
+
+namespace FilmDive.Server.Services.MovieClient
+{
+    public class MovieClientService : IMovieClientService
+    {
+        private readonly HttpClient client;
+
+        public MovieClientService(HttpClient _httpClient)
+        {
+            client = _httpClient;
+        }
+        public async Task<string> SendRequestAsync(string uri, string apiKey)
+        {
+
+            HttpRequestMessage request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(uri),
+                Headers =
+               {
+                    { "accept", "application/json" },
+                    { "Authorization", $"Bearer {apiKey}" },
+               }
+
+            };
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            var body = await response.Content.ReadAsStringAsync();
+            return body;
+        }
+    }
+}
