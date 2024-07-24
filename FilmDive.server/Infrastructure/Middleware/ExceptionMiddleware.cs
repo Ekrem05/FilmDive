43738ec460c9cd9feac2a1d28241b92da1a72d60
Ekrem Beytula <ekremnptg@gmail.com>
2024-07-24 15:39:12 +0300

    Create ExceptionMiddleware.cs

---
+++ FilmDive.server/Infrastructure/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,48 @@
+﻿using FilmDive.Server.ViewModels.Api;
+using System.Net;
+using System.Text.Json;
+
+namespace FilmDive.Server.Infrastructure.Middleware
+{
+    public class ExceptionMiddleware(RequestDelegate next)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = exception switch
+            {
+                ArgumentException => (int)HttpStatusCode.BadRequest,
+                InvalidOperationException => (int)HttpStatusCode.BadRequest,
+                UnauthorizedAccessException => (int)HttpStatusCode.Unauthorized,
+                _ => (int)HttpStatusCode.InternalServerError
+            };
+
+            var response = new ApiResponse<object>
+            {
+                Status = context.Response.StatusCode,
+                Errors = [exception.Message]
+            };
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+
+            var jsonResponse = JsonSerializer.Serialize(response, options);
+
+            return context.Response.WriteAsync(jsonResponse);
+        }
+    }
+}
