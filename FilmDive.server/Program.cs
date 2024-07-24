43320da179ea772688b030d82c61788b90d8f8fa
Ekrem Beytula <ekremnptg@gmail.com>
2024-07-24 14:45:44 +0300

    Update Program.cs

--- FilmDive.server/Program.cs
+++ FilmDive.server/Program.cs
@@ -1,18 +1,43 @@
+using FilmDive.Server.Data;
 using FilmDive.Server.Services.Movie;
 using FilmDive.Server.Services.MovieClient;
 using FilmDive.Server.Services.Movies;
+using FilmDive.Server.Services.Token;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
+var config = builder.Configuration;
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication(opt => {
+    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = config["JwtSettings:Issuer"],
+            ValidAudience = config["JwtSettings:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["JwtSettings:Key"])),
+            ClockSkew = TimeSpan.Zero
+        };
+    });
+builder.Services.AddDbContext<UserContext>(opts =>
+    opts.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
 builder.Services.AddScoped<HttpClient>();
 builder.Services.AddScoped<IMovieService, MovieService>();
-builder.Services.AddScoped<IMovieClientService, MovieClientService>();
+builder.Services.AddScoped<IMovieClientService, MovieClientService>(); 
+builder.Services.AddTransient<ITokenService, TokenService>();
 var app = builder.Build();
 
 app.UseDefaultFiles();
@@ -27,6 +52,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
