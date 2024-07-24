6353024c405f1839d78041426d36cd61ac474a7f
Ekrem Beytula <ekremnptg@gmail.com>
2024-07-24 14:46:04 +0300

    Create Login.cs

---
+++ FilmDive.server/Data/Models/Login.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmDive.Server.Models
+{
+    public class Login
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty; 
+        public string RefreshToken { get; set; } = string.Empty;
+        public DateTime RefreshTokenExpiryTime { get; set; }
+    }
+}
