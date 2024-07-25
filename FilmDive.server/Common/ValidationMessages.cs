b7c6b111b12a942b4a068446eba8fb27e8f53e12
Ekrem Beytula <ekremnptg@gmail.com>
2024-07-25 10:11:10 +0300

    Added password hashing

---
+++ FilmDive.server/Common/ValidationMessages.cs
@@ -0,0 +1,12 @@
+﻿namespace FilmDive.Server.Common
+{
+    public static class ValidationMessages
+    {
+        public static class Cretentials
+        {
+            public const string InvalidPasswordLength = "The password should be between {2} and {1} characters";
+            public const string InvalidUsernameLength = "The username should be between {2} and {1} characters";
+
+        }
+    }
+}
