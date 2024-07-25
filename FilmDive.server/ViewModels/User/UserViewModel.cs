ff7dbd6774807cb0a0cd339f41c24ec4dcf012eb
Ekrem Beytula <ekremnptg@gmail.com>
2024-07-25 10:09:57 +0300

    Added validation attributes to uservm

---
+++ FilmDive.server/ViewModels/User/UserViewModel.cs
@@ -0,0 +1,18 @@
+﻿using static FilmDive.Server.Common.ValidationMessages;
+using System.ComponentModel.DataAnnotations;
+using FilmDive.Server.Infrastructure.Attributes;
+
+namespace FilmDive.Server.ViewModels.User
+{
+    public class UserViewModel
+    {
+        [Required]
+        [StringLength(10, MinimumLength = 2, ErrorMessage = Cretentials.InvalidUsernameLength)]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(20, MinimumLength = 5, ErrorMessage = Cretentials.InvalidPasswordLength)]
+        [MustContainNumber]
+        public string Password { get; set; } = string.Empty;
+    }
+}
