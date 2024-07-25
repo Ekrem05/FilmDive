fa225645f031442c5f9e0bbb6eb831a31c54b466
Ekrem Beytula <ekremnptg@gmail.com>
2024-07-25 13:15:34 +0300

    Update ValidationMessages.cs

--- FilmDive.server/Common/ValidationMessages.cs
+++ FilmDive.server/Common/ValidationMessages.cs
@@ -4,8 +4,8 @@
     {
         public static class Cretentials
         {
-            public const string InvalidPasswordLength = "The password should be between {2} and {1} characters";
-            public const string InvalidUsernameLength = "The username should be between {2} and {1} characters";
+            public const string InvalidPasswordLength = "The Password should be between {2} and {1} characters";
+            public const string InvalidEmailLength = "The Email should not be less than {1} characters";
 
         }
     }
