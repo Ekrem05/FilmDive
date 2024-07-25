3457ca4c8a462980f3ebe202bf480e37456f0019
Ekrem Beytula <ekremnptg@gmail.com>
2024-07-25 13:16:14 +0300

    Create Input.jsx

---
+++ filmdive.client/src/components/Auth/Input.jsx
@@ -0,0 +1,25 @@
+export default function Input({
+  id,
+  label,
+  isValid,
+  errMsg,
+  inputValidations,
+  ...props
+}) {
+  return (
+    <div className="control no-margin">
+      <input
+        {...props}
+        className={`bg-black bg-opacity-30 border ${
+          !isValid ? "border-rose-600" : "border-headersdrk"
+        } text-headersdrk text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+      />
+      <div className="text-rose-600">{!isValid && <p>{errMsg}</p>}</div>
+      <ul style={{ listStyleType: "circle" }} className="text-rose-600 mt-2">
+        {!isValid &&
+          inputValidations &&
+          inputValidations.map((entry) => <li>{entry}</li>)}
+      </ul>
+    </div>
+  );
+}
