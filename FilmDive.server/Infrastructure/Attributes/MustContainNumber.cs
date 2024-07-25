c7b40b368a838cced33bd478a654b5ea022901f5
Ekrem Beytula <ekremnptg@gmail.com>
2024-07-25 10:10:38 +0300

    Custom validation attribute for password

---
+++ FilmDive.server/Infrastructure/Attributes/MustContainNumber.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmDive.Server.Infrastructure.Attributes
+{
+    public class MustContainNumber : ValidationAttribute
+    {
+
+        protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+        {
+            var password = value as string;
+            if (string.IsNullOrEmpty(password) || !password.Any(char.IsDigit))
+            {
+                return new ValidationResult("The password must contain at least one number.");
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
