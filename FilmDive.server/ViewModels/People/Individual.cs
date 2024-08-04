39d51be4d0f9e4b335a8cbeac6cf3691fa659811
Ekrem Beytula <ekremnptg@gmail.com>
2024-08-04 16:50:31 +0300

    Update Individual.cs

--- FilmDive.server/ViewModels/People/Individual.cs
+++ FilmDive.server/ViewModels/People/Individual.cs
@@ -1,9 +1,18 @@
-﻿namespace FilmDive.Server.ViewModels.People
+﻿using Newtonsoft.Json;
+
+namespace FilmDive.Server.ViewModels.People
 {
     public class Individual
     {
         public int Id { get; set; }
 
         public string Name { get; set; } = string.Empty;
+
+        [JsonProperty("known_for_department")]
+        public string Field { get; set; } = string.Empty;
+
+        [JsonProperty("profile_path")]
+        public string ProfilePicture { get; set; }
+
     }
 }
