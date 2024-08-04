e73b99b2a445cb0f3d0fa2b2f29e5d53db5f8519
Ekrem Beytula <ekremnptg@gmail.com>
2024-08-04 16:50:34 +0300

    Update PeopleService.cs

--- FilmDive.server/Services/People/PeopleService.cs
+++ FilmDive.server/Services/People/PeopleService.cs
@@ -10,11 +10,21 @@ namespace FilmDive.Server.Services.People
         public async Task<IEnumerable<Individual>> GetPopularAsync()
         {
             string body = await movieClientService
-              .SendRequestAsync("https://api.themoviedb.org/3/movie/popular?language=en-US&page=1", GetApiKey());
+              .SendRequestAsync("https://api.themoviedb.org/3/person/popular?language=en-US&page=1", GetApiKey());
             var people = JsonConvert.DeserializeObject<ApiRepsone<Individual>>(body);
 
             return people.Result;
         }
+
+        public async Task<IEnumerable<Individual>> SearchAsync(string name)
+        {
+            string body = await movieClientService
+              .SendRequestAsync($"https://api.themoviedb.org/3/search/person?query={name}&include_adult=false&language=en-US&page=1", GetApiKey());
+            var people = JsonConvert.DeserializeObject<ApiRepsone<Individual>>(body);
+            var filteredPeople = people.Result.Where(individual => !string.IsNullOrEmpty(individual.ProfilePicture));
+            return filteredPeople;
+        }
+
         private string GetApiKey()
         {
             return configuration.GetValue<string>("FilmDive");
