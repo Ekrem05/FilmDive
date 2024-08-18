0ccc3dbbbc2dc758dc811ff96f632486973355bb
Ekrem Beytula <78847462+Ekrem05@users.noreply.github.com>
2024-08-18 18:14:21 +0300

    Update Card.jsx

--- filmdive.client/src/components/MovieCard/Card.jsx
+++ filmdive.client/src/components/MovieCard/Card.jsx
@@ -3,15 +3,15 @@ import Imdb from "../Imdb/Imdb";
 import { Link } from "react-router-dom";
 import placeholder from "../../assets/MoviePlaceholder.jpg";
 import LazyImage from "../Image/LazyImage";
-export default function Card({ subject, movie }) {
+export default function Card({ subject, movie, lazyLoad }) {
   return (
     <Link to={`/${subject}/${movie.id}`} className="w-32 2xl:w-full xl:w-60">
       <motion.li
         className="flex flex-col hover:bg-primary p-4 transition-colors duration-200  rounded-xl pb-2 hover:cursor-pointer "
         whileHover={{ scale: 0.9 }}
       >
-        <LazyImage path={movie.posterPath} />
-        <p className=" text-primaryText lg:text-lg w-32 text-sm xl:w-64 text-center overflow-ellipsis overflow-hidden whitespace-nowrap pl-3 pr-3">
+        <LazyImage path={movie.posterPath} lazyLoad={lazyLoad} />
+        <p className=" text-primaryText lg:text-lg text-sm xl:w-64 text-center overflow-ellipsis overflow-hidden whitespace-nowrap pl-3 pr-3">
           {movie.title}
         </p>
         <div className="w-[100%] flex justify-between items-center pr-3">
